99c63c26d4b6d4fdb98e80561af0e9602e0b58df
Timothy McCarthy <ebh042@gmail.com>
2021-03-08 10:28:10 +1100

    Register mUnit test framework

--- plugin/src/main/scala/au/id/tmm/sbt/DependencySettings.scala
+++ plugin/src/main/scala/au/id/tmm/sbt/DependencySettings.scala
@@ -1,6 +1,6 @@
 package au.id.tmm.sbt
 
-import sbt.Keys.libraryDependencies
+import sbt.Keys.{libraryDependencies, testFrameworks}
 import sbt._
 
 object DependencySettings {
@@ -8,12 +8,13 @@ object DependencySettings {
   val silencerVersion = "1.7.1"
   val mUnitVersion    = "0.7.22"
 
-  val commonDependencies: Seq[Def.Setting[Seq[ModuleID]]] = Seq(
+  val commonDependencies = Seq(
     libraryDependencies += "org.scalameta"   %% "munit"        % mUnitVersion    % Test,
     libraryDependencies += "com.github.ghik" %% "silencer-lib" % silencerVersion % Provided cross CrossVersion.full,
     libraryDependencies += compilerPlugin(
       "com.github.ghik" %% "silencer-plugin" % silencerVersion cross CrossVersion.full,
     ),
+    testFrameworks += new TestFramework("munit.Framework"),
   )
 
 }
